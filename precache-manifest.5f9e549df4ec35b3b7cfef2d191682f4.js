6e9d751585d0a69daf81d1bbf4c7c0c72bfad94d
Rogers Ntumwa <ntumwar@gmail.com>
2020-06-20 00:44:44 +0400

    Updates

---
+++ precache-manifest.5f9e549df4ec35b3b7cfef2d191682f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5ecc8292d64a70bca8b1387dae8179",
+    "url": "/ReactMonster-Rolodex/index.html"
+  },
+  {
+    "revision": "2b40c8963f7f0cbce440",
+    "url": "/ReactMonster-Rolodex/static/css/main.9acb213a.chunk.css"
+  },
+  {
+    "revision": "71051572b7a42ea0fbf2",
+    "url": "/ReactMonster-Rolodex/static/js/2.2524815f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMonster-Rolodex/static/js/2.2524815f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b40c8963f7f0cbce440",
+    "url": "/ReactMonster-Rolodex/static/js/main.cc993bd8.chunk.js"
+  },
+  {
+    "revision": "2bcf572b9a2e920b3a0d",
+    "url": "/ReactMonster-Rolodex/static/js/runtime-main.baab682c.js"
+  }
+]);
